5ae819b3174cdb831ad21b88a004d25cb2a0a15c
Arnaud Didry <arnaud@didry.info>
2014-08-09 21:52:35 +0200

    feat(demo): Style & co

--- demo/app/scripts/directives/ixCodeExample.js
+++ demo/app/scripts/directives/ixCodeExample.js
@@ -4,10 +4,50 @@ angular.module('demoApp')
   .directive('code', function () {
     return {
       restrict: 'A',
-      priority: 2000,
       link: function postLink(scope, element, attrs) {
-        element.text(element.html());
-        Rainbow.color();
+        // element.text(element.html());
+      }
+    };
+  });
+
+
+angular.module('demoApp')
+  .directive('ixExample', function () {
+    return {
+      restrict: 'A',
+      // priority: 999000,
+      link: function postLink(scope, element, attrs) {
+        var content = element.html(),
+            lang = element.is('script') ? 'javascript' : 'html',
+            indentation,
+            container,
+            method;
+
+        if (/ix-example-start/.test(content)) {
+          content = content.replace(/(.|\n)*ix-example-start.*\n/m, '');
+        }
+
+        if (/ix-example-end/.test(content)) {
+          content = content.replace(/\n.*ix-example-end(.|\n)*$/m, '');
+        }
+
+        content = content.replace(/^(\n *\n|( *\n))*/g, '');
+        indentation = content.search(/[^ ]/);
+
+        if (attrs['ixExample']) {
+          container = angular.element(attrs['ixExample']);
+          method = 'append'
+        }
+        else {
+          container = element;
+          method = 'before';
+        }
+
+        if (indentation > 0) {
+          content = content.replace(new RegExp('(^|\n)[ ]{1,' + indentation + '}', 'gm'), '$1');
+        }
+
+        container[method]('<pre><code data-language="' + lang + '">' + content + '</code></pre>');
       }
     };
   });
