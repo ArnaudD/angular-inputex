add0d28978497c06810ff6aa14edccae247b62a5
Arnaud Didry <arnaud@didry.info>
2014-08-14 03:21:44 +0200

    doc(demo): i18n

--- demo/app/scripts/app.js
+++ demo/app/scripts/app.js
@@ -3,6 +3,14 @@
 angular.module('demoApp', [
   'ngSanitize',
   'ngRoute',
+  'pascalprecht.translate',
   'ix',
   'ix.templates'
-]);
+])
+.config(function($translateProvider) {
+  $translateProvider.useStaticFilesLoader({
+    prefix: 'bower_components/angular-inputex/dist/angular-inputex-',
+    suffix: '.json'
+  });
+  $translateProvider.preferredLanguage('en_GB');
+});
